c05a5b105f6b439503fd55bfa41b58c8aa83d0fc
lugasparihukamasandika-crypto <lugasparihukamasandika@gmail.com>
2026-05-11 17:55:02 +0700

    fix: implement pause and resume state

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,38 +1,106 @@
 using UnityEngine;
+    using UnityEngine.SceneManagement;
 
-public class GameManager : MonoBehaviour
-{
-    public static GameManager Instance;
-
-    public GameState currentState;
-
-    void Awake()
+    public class GameManager : MonoBehaviour
     {
-        Instance = this;
-    }
+        public static GameManager Instance;
 
-    void Start()
-    {
-        currentState = GameState.Playing;
-    }
+        public GameState currentState;
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        [Header("UI Panel")]
+        [SerializeField] private GameObject pausePanel;
+        [SerializeField] private GameObject gameOverPanel;
+
+        void Awake()
         {
-            PauseGame();
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        void Start()
+        {
+            ChangeState(GameState.Playing);
+
+            pausePanel.SetActive(false);
+            gameOverPanel.SetActive(false);
+        }
+
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (currentState == GameState.Playing)
+                {
+                    PauseGame();
+                }
+                else if (currentState == GameState.Paused)
+                {
+                    ResumeGame();
+                }
+            }
+        }
+
+        public void ChangeState(GameState newState)
+        {
+            currentState = newState;
+
+            switch (newState)
+            {
+                case GameState.Playing:
+                    Time.timeScale = 1f;
+
+                    pausePanel.SetActive(false);
+                    gameOverPanel.SetActive(false);
+                    break;
+
+                case GameState.Paused:
+                    Time.timeScale = 0f;
+
+                    pausePanel.SetActive(true);
+                    break;
+
+                case GameState.GameOver:
+                    Time.timeScale = 0f;
+
+                    gameOverPanel.SetActive(true);
+                    break;
+            }
+        }
+
+        public void PauseGame()
+        {
+            ChangeState(GameState.Paused);
+        }
+
+        public void ResumeGame()
+        {
+            ChangeState(GameState.Playing);
+        }
+
+        public void GameOver()
+        {
+            ChangeState(GameState.GameOver);
+        }
+
+        public void RestartGame()
+        {
+            Time.timeScale = 1f;
+
+            SceneManager.LoadScene(
+                SceneManager.GetActiveScene().buildIndex
+            );
+        }
+
+        public void BackToMenu()
+        {
+            Time.timeScale = 1f;
+
+            SceneManager.LoadScene("MainMenu");
         }
     }
-
-    public void PauseGame()
-    {
-        Time.timeScale = 0f;
-        currentState = GameState.Paused;
-    }
-
-    public void GameOver()
-    {
-        Debug.Log("Game Over");
-        currentState = GameState.GameOver;
-    }
-}
